54a88d03af7347181e7ef335f7887123b942a605
Adriana Cividini <121236952+AdrianaCividini@users.noreply.github.com>
2024-07-03 17:12:00 +0200

    Building gaming cards.

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,12 @@ function App() {
   return (
     <Grid
       templateAreas={{
-        base: `"nav nav"`,
+        base: `"nav main"`,
         lg: `"nav nav" "aside main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "250px 1fr",
       }}
     >
       <GridItem area="nav">
